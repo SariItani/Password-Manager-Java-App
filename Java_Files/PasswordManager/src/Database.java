623c10d5fe02151486cdea100ff6730fa12d983a
Leboweeb <74187782+Leboweeb@users.noreply.github.com>
2023-04-16 20:10:10 +0300

    add database internal class

--- Java_Files/PasswordManager/src/Database.java
+++ Java_Files/PasswordManager/src/Database.java
@@ -1,3 +1,46 @@
+import java.io.*;
+
 public class Database {
-    
+
+    final String DATABASE_NAME = "database.dat";
+
+    public void storetoDB(String encryptedString) {
+        try {
+            appendToDataBase(DATABASE_NAME, encryptedString);
+        } catch (IOException e) {
+            System.out.println("Couldn't store password in database");
+        }
+    }
+
+    public String getPasswordfromDB(String encryptedString) {
+        String temp = "";
+        try {
+            temp = getFromDataBase(DATABASE_NAME, encryptedString);
+        } catch (IOException e) {
+            System.out.println("Couldn't get password from database");
+        }
+        return temp;
+    }
+
+    private static void appendToDataBase(String filename, String data)
+            throws IOException {
+        RandomAccessFile file = new RandomAccessFile(filename, "rw");
+        if (file.length() > 0)
+            file.seek(file.length()); // append to a dat file
+        file.writeUTF(data);
+        file.close();
+    }
+
+    private static String getFromDataBase(String filename, String pass) throws IOException {
+        RandomAccessFile file = new RandomAccessFile(filename, "r");
+        String match = "";
+        while (true) {
+            match = file.readUTF();
+            if (match.equals(pass))
+                break;
+
+        }
+        file.close();
+        return match;
+    }
 }
---
+++ test/DatabaseTest.java
@@ -0,0 +1,8 @@
+
+public class DatabaseTest {
+    public static void main(String[] args) {
+        String s = "ass";
+        Database db = new Database();
+        db.storetoDB(s);
+    }
+}
