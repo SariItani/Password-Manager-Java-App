c5a93aba3ada68c527889a4ff881d9b034ac8b10
SariItani <74187768+SariItani@users.noreply.github.com>
2023-04-16 15:32:12 +0300

    Set the Java Project

---
+++ Java_Files/PasswordManager/src/MainManager.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MainManager
+{
+    
+    public static void main(String []args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Suck your mamas tits");
+        System.out.println("Enter your favorit msabbe: ");
+        String msabbe = input.nextLine();
+        System.out.println("Your Favorite msabbe is: " + msabbe);
+        input.close();
+    }    
+}
