d6ee19012a2125ab8978af8b4d9901d4bdad05da
SariItani <74187768+SariItani@users.noreply.github.com>
2023-04-16 20:23:56 +0300

    .

--- Java_Files/PasswordManager/src/Encrypter.java
+++ Java_Files/PasswordManager/src/Encrypter.java
@@ -1,8 +1,36 @@
+import java.util.logging.Logger;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.XMLFormatter;
 public class Encrypter
-{
-    
+{  
+    private final static Logger log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+    public void log_message(String message)
+    {
+        log.log(Level.INFO, message);
+    }
+
     public static void main(String[] args)
     {
-        
+        XMLFormatter xmlFormatter = new XMLFormatter();
+        LogRecord logRecord = new LogRecord(Level.INFO, "Logrecord message to be printed in xml file..");
+        FileHandler fileHandler;
+        try {
+            fileHandler = new FileHandler("logrecordxml.xml");
+            fileHandler.setFormatter(xmlFormatter);
+            fileHandler.publish(logRecord);
+            fileHandler.flush();
+        } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Encrypter e0 = new Encrypter();
+        e0.log_message("HELL YEAH");
     }
 }
