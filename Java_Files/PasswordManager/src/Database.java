175659a0d1ee6a921654816164fed4b9c550cb8c
SariItani <74187768+SariItani@users.noreply.github.com>
2023-04-16 16:10:27 +0300

    .

--- Java_Files/PasswordManager/src/Database.java
+++ Java_Files/PasswordManager/src/Database.java
@@ -1,3 +1,14 @@
-public class Database {
-    
+import java.awt.datatransfer.StringSelection;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+
+public class Database
+{
+    public static void main(String []args)
+    {
+        String myString = "Feck Yur Mother";
+        StringSelection stringSelection = new StringSelection(myString);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+    }
 }
