a2158568609d6670cf92879c98996729e20b018f
Leboweeb <74187782+Leboweeb@users.noreply.github.com>
2023-04-22 08:44:30 +0300

    I forgor

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 # get the project from github
 git clone https://github.com/SariItani/Password-Manager-Java-App.git
 # generate the manifest file
 printf "Main-Class : TerminalInterface\nClass-Path: bin/\n" > Manifest.txt
 DIR=Password-Manager-Java-App
-# generate a random key before building the files
-./$DIR/generate_key.sh
-# build it after randomizing the key
-cd $DIR || return # return early if we can't change directories for some reason
 CLASSPATH=~/Downloads/jna-5.13.0.jar:~/Downloads/jna-platform-5.13.0.jar:$CLASSPATH
 mv Manifest.txt $DIR/
+cd $DIR
+# generate a random key before building the files
+./generate_key.sh
+# build it after randomizing the key
 javac -d Java_Files/PasswordManager/bin Java_Files/PasswordManager/src/*.java
 jar cvfm PasswordManager.jar Manifest.txt -C Java_Files/PasswordManager/bin .
 java -jar PasswordManager.jar
