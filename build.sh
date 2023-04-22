605f056a55bc91329feafffa2f58ea208280a57b
Leboweeb <74187782+Leboweeb@users.noreply.github.com>
2023-04-22 08:55:12 +0300

    oracle can't write code properly. Fuck you oracle

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # get the project from github
 git clone https://github.com/SariItani/Password-Manager-Java-App.git
 # generate the manifest file
-printf "Main-Class : TerminalInterface\nClass-Path: bin/\n" > Manifest.txt
+printf "Main-Class: TerminalInterface\nClass-Path: bin/\n" > Manifest.txt
 DIR=Password-Manager-Java-App
 CLASSPATH=~/Downloads/jna-5.13.0.jar:~/Downloads/jna-platform-5.13.0.jar:$CLASSPATH
 mv Manifest.txt $DIR/
