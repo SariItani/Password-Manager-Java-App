ec9b3d206538511e52207a966fcfd307ba8bbd62
Leboweeb <74187782+Leboweeb@users.noreply.github.com>
2023-04-22 09:02:54 +0300

    update yet again

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# get the project from github
+# get the project from github as well as the required libs
 git clone https://github.com/SariItani/Password-Manager-Java-App.git
+curl https://repo1.maven.org/maven2/net/java/dev/jna/jna/5.13.0/jna-5.13.0.jar --output jna-5.13.0.jar
+curl https://repo1.maven.org/maven2/net/java/dev/jna/jna-platform/5.13.0/jna-platform-5.13.0.jar --output jna-platform-5.13.0.jar
 # generate the manifest file
 printf "Main-Class: TerminalInterface\nClass-Path: bin/\n" > Manifest.txt
 DIR=Password-Manager-Java-App
-CLASSPATH=~/Downloads/jna-5.13.0.jar:~/Downloads/jna-platform-5.13.0.jar:$CLASSPATH
+CLASSPATH=./jna-5.13.0.jar:./jna-platform-5.13.0.jar:$CLASSPATH
 mv Manifest.txt $DIR/
 cd $DIR
 # generate a random key before building the files
